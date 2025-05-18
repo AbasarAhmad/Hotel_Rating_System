8865e9c19c9829466741fb3c642c900d32f3044e
Abasar Ahmad <abasar.a@protectt.ai>
2025-05-18 23:18:55 +0530

    Rating Service project is initialized

---
+++ RatingService/src/test/java/com/saar/rating/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saar.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
